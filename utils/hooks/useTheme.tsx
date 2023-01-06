13d8faf75fa37e321cc214cbd44a25928c9f5771
Pablo Vega Uceta <pvega@newrelic.com>
2023-01-06 19:50:35 +0100

    🐛 Fix: Theme when starting the app

--- utils/hooks/useTheme.tsx
+++ utils/hooks/useTheme.tsx
@@ -7,18 +7,8 @@ interface AppState {
 const defaultTheme = 'light'
 
 export const useTheme = () => {
-  const [theme, setTheme] = useState<AppState['theme']>(defaultTheme)
-
-  useEffect(() => {
-    const localStorageTheme = window.localStorage.getItem('theme') || undefined
-
-    if (localStorageTheme) {
-      document.body.dataset.theme = localStorageTheme
-      setTheme(localStorageTheme)
-    } else {
-      setTheme(defaultTheme)
-    }
-  }, [])
+  const localStorageTheme = window.localStorage.getItem('theme') || defaultTheme
+  const [theme, setTheme] = useState<AppState['theme']>(localStorageTheme)
 
   useEffect(() => {
     document.body.dataset.theme = theme
